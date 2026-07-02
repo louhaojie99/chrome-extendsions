ecad81bc93618106d3d00c1f1080b5dd21da55dc
louhaojie99 <851681631@qq.com>
2026-07-02 16:55:48 +0800

    feat: add tabs-manager plugin

---
+++ tabs-manager/popup.js
@@ -0,0 +1,15 @@
+const tabs = await chrome.tabs.query({
+  url: [
+    "https://developer.chrome.com/docs/webstore/*",
+    "https://developer.chrome.com/docs/extensions/*",
+  ]
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  if (tabIds.length) {
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: "DOCS" });
+  }
+});
